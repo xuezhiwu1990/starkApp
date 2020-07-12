382f0c7b142cc5868304428c9eac865bbbfb3b4d
e <15117995393@126.com>
2020-07-12 22:42:59 +0800

    ccc

--- core/config_web/database.php
+++ core/config_web/database.php
@@ -1,7 +1,7 @@
 <?php
 return [
 	//mysql 配置文件
-	'MYSQL_DSN' => 'mysql:host=127.0.0.1;dbname=zhiduApp',
+	'MYSQL_DSN' => 'mysql:host=127.0.0.1;dbname=test',
 	'MYSQL_USER' => 'root',
 	'MYSQL_PWD' => 'zy126***',
 	
