c38ee20198e37b2726700b820c6beb2b6d073a79
e <15117995393@126.com>
2020-07-15 00:20:29 +0800

    database

--- core/config_web/database.php
+++ core/config_web/database.php
@@ -1,8 +1,18 @@
 <?php
 return [
 	//mysql 配置文件
-	'MYSQL_DSN' => 'mysql:host=127.0.0.1;dbname=test',
-	'MYSQL_USER' => 'root',
-	'MYSQL_PWD' => 'zy126***',
-	
+	'PDO' => [
+		'MYSQL_DSN' => 'mysql:host=127.0.0.1;dbname=test',
+		'MYSQL_USER' => 'root',
+		'MYSQL_PWD' => 'zy126***',
+	],
+	'MEDOO' => [
+		'database_type' => 'mysql',
+		'database_name' => 'test',
+		'server' => '127.0.0.1',
+		'username' => 'root',
+		'password' => 'root',
+		'charset' => 'zy126***',
+		'port' => 3306,
+	],
 ];
